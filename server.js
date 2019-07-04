8d025cbf65e97a5d84a80315e6b87963e00aeb99
DMa <davidweibinma@Weibins-MacBook-Pro-2.local>
2019-07-03 20:30:13 -0400

    minor update

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater";
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, {useFindAndModify: false});
 
 // Routes
 
